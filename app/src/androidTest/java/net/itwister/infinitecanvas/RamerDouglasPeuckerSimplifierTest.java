fd8825bc5d82ada2804158a4485bf5255bfa5d50
y2k <itwisterlx@gmail.com>
2015-01-21 09:41:36 -0800

    тесты

--- app/src/androidTest/java/net/itwister/infinitecanvas/RamerDouglasPeuckerSimplifierTest.java
+++ app/src/androidTest/java/net/itwister/infinitecanvas/RamerDouglasPeuckerSimplifierTest.java
@@ -1,6 +1,6 @@
 package net.itwister.infinitecanvas;
 
-import android.graphics.Point;
+import android.graphics.PointF;
 import android.test.AndroidTestCase;
 
 /**
@@ -8,32 +8,42 @@ import android.test.AndroidTestCase;
  */
 public class RamerDouglasPeuckerSimplifierTest extends AndroidTestCase {
 
-    public void test() {
-        Point[] testPoints = new Point[] {
-                new Point(0, 0),
-                new Point(50, 0),
-                new Point(100, 0),
-                new Point(100, 50),
-                new Point(100, 100),
-                new Point(50, 100),
-                new Point(0, 100),
-                new Point(0, 50),
-                new Point(0, 0),
+    static PointF[] TEST_POINTS = new PointF[] {
+            new PointF(0, 0),
+            new PointF(50, 0),
+            new PointF(100, 0),
+            new PointF(100, 50),
+            new PointF(100, 100),
+            new PointF(50, 100),
+            new PointF(0, 100),
+            new PointF(1, 1),
+    };
+
+    private RamerDouglasPeuckerSimplifier simplifier;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        simplifier = new RamerDouglasPeuckerSimplifier();
+        simplifier.setPoints(TEST_POINTS);
+    }
+
+    public void testEpsilonZero() {
+        simplifier.setEpsilon(1.0f);
+        PointF[] actual = simplifier.compute();
+        PointF[] expected = new PointF[] {
+                new PointF(0, 0),
+                new PointF(100, 0),
+                new PointF(100, 100),
+                new PointF(0, 100),
+                new PointF(1, 1),
         };
+        assertCollections(expected, actual);
+    }
 
-        RamerDouglasPeuckerSimplifier simplifier = new RamerDouglasPeuckerSimplifier();
-        simplifier.setEpsilon(0.1f);
-        simplifier.setPoints(testPoints);
-        Point[] actual = simplifier.compute();
-
-        Point[] expected = new Point[] {
-                new Point(0, 0),
-                new Point(100, 0),
-                new Point(100, 100),
-                new Point(0, 100),
-                new Point(0, 0),
-        };
-
-        assertEquals(expected, actual);
+    private void assertCollections(PointF[] expected, PointF[] actual) {
+        assertEquals(expected.length, actual.length);
+        for (int i = 0; i < actual.length; i++)
+            assertEquals(expected[i], actual[i]);
     }
 }
--- app/src/main/java/net/itwister/infinitecanvas/RamerDouglasPeuckerSimplifier.java
+++ app/src/main/java/net/itwister/infinitecanvas/RamerDouglasPeuckerSimplifier.java
@@ -1,6 +1,11 @@
 package net.itwister.infinitecanvas;
 
 import android.graphics.Point;
+import android.graphics.PointF;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created on 1/21/2015.
@@ -8,17 +13,65 @@ import android.graphics.Point;
 public class RamerDouglasPeuckerSimplifier {
 
     private float epsilon;
-    private Point[] points;
+    private PointF[] points;
 
     public void setEpsilon(float epsilon) {
         this.epsilon = epsilon;
     }
 
-    public void setPoints(Point[] points) {
+    public void setPoints(PointF[] points) {
         this.points = points;
     }
 
-    public Point[] compute() {
-        return new Point[0];
+    public PointF[] compute() {
+        List<PointF> result = douglasPeucker(Arrays.asList(points), 0, points.length - 1, epsilon / 20);
+        return result.toArray(new PointF[result.size()]);
+    }
+
+    private static List<PointF> douglasPeucker(List<PointF> points, int startIndex, int lastIndex, float epsilon) {
+        float dmax = 0f;
+        int index = startIndex;
+
+        for (int i = index + 1; i < lastIndex; ++i) {
+            float d = pointLineDistance(points.get(i), points.get(startIndex), points.get(lastIndex));
+            if (d > dmax) {
+                index = i;
+                dmax = d;
+            }
+        }
+
+        if (dmax > epsilon) {
+            List<PointF> res1 = douglasPeucker(points, startIndex, index, epsilon);
+            List<PointF> res2 = douglasPeucker(points, index, lastIndex, epsilon);
+
+            List<PointF> finalRes = new ArrayList<>();
+            for (int i = 0; i < res1.size() - 1; ++i) {
+                finalRes.add(res1.get(i));
+            }
+
+            for (int i = 0; i < res2.size(); ++i) {
+                finalRes.add(res2.get(i));
+            }
+
+            return finalRes;
+        }
+        else {
+            return Arrays.asList(points.get(startIndex), points.get(lastIndex));
+        }
+    }
+
+    private static float pointLineDistance(PointF point, PointF start, PointF end) {
+        if (start == end) {
+            return distance(point, start);
+        }
+
+        float n = Math.abs((end.x - start.x) * (start.y - point.y) - (start.x - point.x) * (end.y - start.y));
+        double d = Math.sqrt((end.x - start.x) * (end.x - start.x) + (end.y - start.y) * (end.y - start.y));
+        return (float) (n / d);
+    }
+
+    private static float distance(PointF value1, PointF value2)
+    {
+        return (float)Math.sqrt((value1.x - value2.x) * (value1.x - value2.x) + (value1.y - value2.y) * (value1.y - value2.y));
     }
 }
